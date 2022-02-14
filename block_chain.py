9a855aa3841b18ec2f4f3dcafbe8a05dd0b7759c
blank-knight <846735605@qq.com>
2022-02-14 21:44:57 +0800

    master

--- block_chain.py
+++ block_chain.py
@@ -1,55 +1,150 @@
 import hashlib
 import datetime
+import os
+from sys import int_info
+from Crypto import PublicKey
+from Crypto.Hash import SHA256
+from Crypto.Signature import DSS
+from Crypto.PublicKey import ECC
+
+def diffculty(diff):
+    res = ""
+    for i in range(diff):
+        res += "0"
+    return res
+
+# 公私钥的生成和签名对象的生成(模拟钱包节点)
+class wallet:
+    def __init__(self) -> None:
+        self.key = ECC.generate(curve='P-256') # 生成ECC密钥对
+        self.signer = DSS.new(self.key, 'fips-186-3') # 生成签名对象
+
+    def __repr__(self) -> str:
+        # 钱包地址是对公钥进行hash生成的
+        # 不直接用公钥做地址是为了安全起见
+        return "Wallet_address:"+SHA256.new((self.key.public_key()).encode()).hexdigest()
 
 class block:
-    def __init__(self,data,pre_hash) -> None:
+    # 交易信息,前一个区块的hash,矿工地址,交易发起者(交易签名者)
+    def __init__(self,transaction,pre_hash,wallet="",miner_address="赵文韬") -> None:
         self.loc = 1
-        self.data = data  
-        self.pre_hahs = pre_hash
-        self.hash = self.com_hash(pre_hash)
+        self.diff = 1 # 当前区块的挖矿难度(前面要求几个0)
+        self.pre_hash = pre_hash
+        self.timestamp = datetime.datetime.now()
+        self.hash = None
+        self.reward = 10 # 挖矿成功的奖励,由当前区块发放
+        self.wallet = wallet
+        self.transaction = transaction
+        self.miner_reward = self.mine(transaction,miner_address) # 开始挖矿
         self.nex_block = None
+        
 
-    def com_hash(self,pre_hash):
-        val = self.data+pre_hash
+    def __repr__(self) -> str:
+        return "loc = "+str(self.loc)+"\ntime = "+str(self.timestamp)+"\ntransaction = "\
+            +self.transaction+"\nminer_reward="+self.miner_reward+"\npre_hash = "+self.pre_hash+"\nhash = "+self.hash+"\n"
+        
+    def com_hash(self,transaction):
+        sig_hash = SHA256.new((transaction+self.pre_hash).encode()).hexdigest()
+        val = sig_hash+str(self.nonce)+str(self.timestamp)
         return hashlib.sha256(val.encode('utf-8')).hexdigest()
+
+
+    # 挖矿
+    def mine(self,transaction,miner_address):
+        res = self.validation() if self.wallet != "" else "" # 开始验证是否是有效的交易
+        if res == 0:
+            return 0
+        self.nonce = 1
+        pre_string = diffculty(self.diff)
+        while(True):
+            if self.com_hash(transaction)[0:self.diff] != pre_string[0:self.diff]:
+                self.nonce += 1
+                continue
+            else:
+                self.hash = self.com_hash(transaction)
+                return self.hash+" "+miner_address+" "+str(self.reward)
+
+    # 模拟发送,接受和验证
+    def validation(self):
+        self.tra_hash = SHA256.new((self.transaction+self.pre_hash).encode()) # 对交易信息和前一个区块的hash再进行hash
+        self.sign_obj = wallet.signer.sign(self.tra_hash) if wallet!="" else self.transaction # 对得到的hash进行签名
+        with open('sign.bin', 'wb') as f: # 将签名写入文件，模拟发送（同时还发送了文本内容，为了方便，不写文件，后面直接引用）
+            f.write(self.sign_obj)
+        with open('sign.bin', 'rb') as f: # 读取签名内容，模拟接收
+            sign_new = f.read()
+            # sign_new = bytearray(f.read())  改为可修改的二进制数组,但这里有问题,不能用这个
+        # 接受者对象
+        '''
+            测试修改二进制文件,看是否能发现
+        '''
+        # a=123
+        # b=a.to_bytes(1,'big')
+        # sign_new += b
+        verifer = DSS.new(self.wallet.key.public_key(), 'fips-186-3') 
+        hasher = SHA256.new((self.transaction+self.pre_hash).encode()) # 对收到的文件进行hash
+        try:
+            verifer.verify(hasher,sign_new) # 校验摘要（本来的样子）和收到并解密的签名是否一致
+            # print("签名有效")
+        except:
+            print("签名无效,停止挖矿")
+            return 0
+
 
 class chain:
     def __init__(self) -> None:
         self.first_block = self.fir_block()
         self.length = 1
+        self.transactionPool = []
+
+    # fro为发送者公钥,to为接受这公钥,amount为交易具体信息
+    # signer 为签名对象
+    def add_Transaction(self,fro,to,amount):
+        if len(fro.strip()) == 0 or len(fro.strip()) == 0: # 交易地址不能为空和空格
+            print("交易地址错误,请重新输入")
+            return 0
+        if amount <= 0:
+            print("无效的交易金额,请重新输入") # 交易金额不能为负或者0
+            return 0
+        string = fro+" "+to+" "+str(amount)
+        # 交易信息直接公开,可以先不用签名.
+        self.transactionPool.append(string) # 有效的交易加入交易池等待矿工来挖矿
 
     def fir_block(self):
-        f_blcok = block("I am first blcok","")
+        f_blcok = block("I am first blcok","") 
         return f_blcok
     
-    def find_cur_block(self):
+    def find_pre_block(self):
         if self.length == 2:
             return self.first_block.nex_block
-        self.cur_block = self.first_block.nex_block
+        self.pre_block = self.first_block.nex_block
         for i in range(self.length-2):
-            self.cur_block = self.cur_block.nex_block
-        return self.cur_block
+            self.pre_block = self.pre_block.nex_block
+        return self.pre_block
 
+    # 输出区块链的信息
     def output_chain_info(self):
         self.cur_block = self.first_block
         for i in range(self.length):
-            dic1 = self.cur_block.__dict__
-            print('\n'.join(('%s:%s' % item for item in dic1.items())))
+            print(self.cur_block)
+            # dic1 = self.cur_block.__dict__
+            # print('\n'.join(('%s:%s' % item for item in dic1.items())))
             self.cur_block = self.cur_block.nex_block
 
     # 我这里是通过指针连接的,也可以使用堆栈进行顺序连接
-    # 添加区块的方式1:直接添加数据
-    def add_block_1(self,data):
+    def add_block(self,wallet,transaction=None):
+        transaction = self.transactionPool.pop(0)
         if self.length == 1:
-            self.first_block.nex_block = block(data,self.first_block.hash)
+            # print(type(transaction))
+            # print(type(self.first_block.hash))
+            self.first_block.nex_block = block(transaction,self.first_block.hash,wallet)
             self.first_block.nex_block.loc += 1
             self.length += 1
         else:
-            self.cur_block = self.find_cur_block()
-            self.cur_block.nex_block = block(data,self.cur_block.hash)
-            self.cur_block.nex_block.loc += self.cur_block.loc
+            self.pre_block = self.find_pre_block()
+            self.pre_block.nex_block = block(transaction,self.pre_block.hash,wallet)
+            self.pre_block.nex_block.loc += self.pre_block.loc
             self.length += 1
-    
+
     # 获取一个区块
     def get_block(self,num):
         if num == 1:
@@ -62,14 +157,14 @@ class chain:
         return self.length
 
     def validation(self):
-        if self.first_block.hash != self.first_block.com_hash(""): # 验证创世区块
+        if self.first_block.hash != self.first_block.com_hash("I am first blcok"): # 验证创世区块
             print("数据被篡改了")
             return 0
         self.pre_block = self.first_block
         self.cur_block = self.first_block.nex_block # 从创世区块之后开始挨着验证
         for i in range(1,self.length):
-            # 判断当前区块是否被篡改,由于我这里com_hash的计算同时用了账本data和前一区块的hash,所以这里只用一个验证即可,只要其中
-            #一个改了,当前区块的hash就会对不上
+            # 判断当前区块是否被篡改,由于我这里com_hash的计算同时用了账本transaction和前一区块的hash,所以这里只用一个验证即可,只要其中
+            #一个改了,当前区块的hash就会对不上.改hash,左边的会变,改transaction,右边的会变,直接一个验证即可
             if self.cur_block.hash !=  self.cur_block.com_hash(self.pre_block.hash): 
                 print("数据被篡改了")
                 return 0
@@ -79,29 +174,38 @@ class chain:
         print("区块链一切正常")
         
 
-
 if __name__ == "__main__":
     # 测试添加区块
     chain_test = chain()
-    chain_test.add_block_1("给赵文韬转账1000元")
-    chain_test.add_block_1("给赵文韬转账20000元")
-    chain_test.add_block_1("给赵文韬转账30000000元")
+    wallet = wallet()
+    # print(wallet)
+    chain_test.validation()
+    chain_test.add_Transaction("赵文甲","赵文乙",1000)
+    chain_test.add_Transaction("赵文丙","赵文丁",2000)
+    chain_test.add_Transaction("赵文帅","赵文韬",3000)
+    # signer = wallet.signer
+    # a = "fafa"
+    # tra_hash = hashlib.sha256(a.encode('utf-8')).hexdigest() # 对交易信息和前一个区块的hash再进行hash
+    # sign_obj = signer.sign(tra_hash) if signer!="" else 141 # 对得到的hash进行签名
+    chain_test.add_block(wallet)
+    chain_test.add_block(wallet)
+    chain_test.add_block(wallet)
+    # chain_test.add_block("给赵文韬转账20000元")
+    # chain_test.add_block("给赵文韬转账30000000元")
     chain_test.output_chain_info()
+    # input("请按任意键继续")
 
-    # 测试修改账本
+    # 测试修改区块
     block2 = chain_test.get_block(2)
-    print(block2.__dict__)
-    block2.data = "我不转钱了"
+    print(block2)
+    block2.transaction = "我不转钱了"
     chain_test.validation()
-    block1 = chain_test.get_block(1)
-    block1.data = "我是魔神区块"
-    chain_test.validation()
+    # chain_test.output_chain_info()
 
-    # 测试修改hash
-    block2 = chain_test.get_block(2)
+    # print("____________________________________")
+    # # # 测试修改hash
+    block3 = chain_test.get_block(3)
     val = "jojojojo"
     hh = hashlib.sha256(val.encode('utf-8')).hexdigest()
     block2.hash = hh
     chain_test.validation()
-    print(block2.__dict__)
-    print(hh)
--- test.py
+++ test.py
@@ -1,13 +1,47 @@
+from Crypto.PublicKey import ECC
+from Crypto.Hash import SHA256
+from Crypto.Signature import DSS
+ 
+# 生成ECC密钥对
+key = ECC.generate(curve='P-256')
+ke = key.public_key()
+# print("key:",key)
 
+# 待签名内容(发送的文本内容)
+message = 'I am MKing Hello Everyone'
+ 
+# 签名
+signer = DSS.new(key, 'fips-186-3') # 第二个参数代表生成模式，fips-186-3表示签名生成是随机的,用DSS生成签名对象,签名对象本人可进行签名
+hasher = SHA256.new(message.encode()) # 对文本进行Hash，提取内容摘要
+sign_obj = signer.sign(hasher)     #用私钥对消息签名
+ 
 
-class TreeNode:
-    def __init__(self,val) -> None:
-        self.val = val
-        self.ltag = 0
-        self.rtag = 0
-        self.left = None
-        self.right = None
+# print('签名后:', sign_obj)
+# print('签名者:', signer)
+# print('公钥:', key.public_key())
+# print('私钥:', key.has_private())
+# print('export_key:', key)
 
-test = TreeNode(1)
-print(test.__dict__)
-print('\n'.join(('%s:%s' % item for item in dic1.items())))
+ 
+# 将签名写入文件，模拟发送（同时还发送了文本内容，为了方便，不写文件，后面直接引用）
+with open('sign.bin', 'wb') as f:
+    f.write(sign_obj)
+ 
+# 读取签名内容，模拟接收
+with open('sign.bin', 'rb') as f:
+    sign_new = bytearray(f.read())  # 签名内容(二进制)，并转成bytearray，bytearray是可变数组,将数据进行encode编码,可通过decode解码,
+                                    # 相当于b'str',但b'str'是不可变的,这里变为可变的是方便后面模拟错误签名
+ 
+# sign_new.append(0x32)  # 模拟错误的签名
+print('错误签名:', sign_new)
+ 
+# 验证签名
+print("sign_obj:",sign_obj)
+verifer = DSS.new(key.public_key(), 'fips-186-3') # 使用公钥创建校验对象
+hasher = SHA256.new(message.encode()) # 对收到的消息文本提取摘要
+print('hasher:', hasher)
+try:
+    verifer.verify(hasher,sign_new) # 校验摘要（本来的样子）和收到并解密的签名是否一致
+    print("The signature is valid.")
+except (ValueError, TypeError):
+    print("The signature is not valid.")
